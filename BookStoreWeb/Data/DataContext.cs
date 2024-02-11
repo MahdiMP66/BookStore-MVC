37fce088ba97f98ee9a8a2b6627a1b1c2ba5b5c1
mahdimp <madanipour66@gmail.com>
2024-02-11 17:22:35 +0330

    modified Category Table

--- BookStoreWeb/Data/DataContext.cs
+++ BookStoreWeb/Data/DataContext.cs
@@ -5,11 +5,18 @@ namespace BookStoreWeb.Data
 {
     public class DataContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
             
         }
 
-        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category {ID = 1, Name = "Action",DisplayOrder = 2 },
+                new Category { ID = 2, Name = "Drama", DisplayOrder = 2 },
+                new Category { ID = 3, Name = "Story", DisplayOrder = 3 });
+        }
     }
 }
--- BookStoreWeb/Migrations/20240211134107_seedCategoryTable.Designer.cs
+++ BookStoreWeb/Migrations/20240211134107_seedCategoryTable.Designer.cs
@@ -3,6 +3,7 @@ using BookStoreWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookStoreWeb.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240211134107_seedCategoryTable")]
+    partial class seedCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
