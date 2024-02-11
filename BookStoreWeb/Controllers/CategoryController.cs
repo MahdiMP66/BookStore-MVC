dc73dfe9078dcf8e2419acc51550dda1428f543b
mahdimp <madanipour66@gmail.com>
2024-02-11 17:39:56 +0330

    show catogories

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly DataContext _db;
+
+        public CategoryController(DataContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> Categories = _db.Categories.ToList();
+            return View(Categories);
         }
     }
 }
