2487658b07424b27894069e35545311124d8c938
mahdimp <madanipour66@gmail.com>
2024-02-11 16:50:37 +0330

    Database Setup

---
+++ BookStoreWeb/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWeb.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookStoreWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public int DisplayOrder { get; set; }
+    }
+}
