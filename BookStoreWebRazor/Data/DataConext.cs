a49fae9d90eee03c50a64a745b432e4a1f79f642
mahdimp <madanipour66@gmail.com>
2024-02-13 12:09:15 +0330

    added Category and EF core

---
+++ BookStoreWebRazor/Data/DataConext.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreWebRazor.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreWebRazor.Data
+{
+    public class DataConext : DbContext
+    {
+        public DbSet<Category> Categories{ get; set; }
+        public DataConext(DbContextOptions<DataConext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { ID = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { ID = 2, Name = "Horor", DisplayOrder = 12 },
+                new Category { ID = 3, Name = "Razor", DisplayOrder = 3 });
+        }
+    }
+}
---
+++ BookStoreWebRazor/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookStoreWebRazor.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        [DisplayName("Category Name")]
+        //[MaxLength(20,ErrorMessage = "should be less than 20 letters")]
+        [MaxLength(20)]
+        public string Name { get; set; } = string.Empty;
+        [DisplayName("Display Order")]
+        [Range(1, 100)]
+        public int DisplayOrder { get; set; }
+    }
+}
