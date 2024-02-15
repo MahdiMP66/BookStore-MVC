265a57966efb44a4a6d53007569113d68a04b965
mahdimp <madanipour66@gmail.com>
2024-02-16 00:33:05 +0330

    product repository

---
+++ BookStoreDataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.DataAccess.IRepository;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly DataContext _db;
+
+        public ProductRepository(DataContext db) : base(db) 
+        {
+           _db = db;
+        }
+        public void Save()
+        {
+
+            _db.SaveChanges();
+        }
+
+        public void Update(Product product)
+        {
+            _db.Update(product);  
+        }
+
+    }
+}
