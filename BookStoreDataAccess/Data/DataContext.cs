fcd33ecbb15c1e926e08f3218191bd9f738b743c
mahdimp <madanipour66@gmail.com>
2024-02-16 00:24:35 +0330

    added product to database

--- BookStoreDataAccess/Data/DataContext.cs
+++ BookStoreDataAccess/Data/DataContext.cs
@@ -6,6 +6,7 @@ namespace BookStore.DataAccess
     public class DataContext : DbContext
     {
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
             
@@ -17,6 +18,46 @@ namespace BookStore.DataAccess
                 new Category {ID = 1, Name = "Action",DisplayOrder = 2 },
                 new Category { ID = 2, Name = "Drama", DisplayOrder = 2 },
                 new Category { ID = 3, Name = "Story", DisplayOrder = 3 });
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "Action",
+                    ISBN = "1",
+                    Description = "desc1"
+                    ,
+                    Author = "mahdi1",
+                    ListPrice = 50,
+                    Price = 50,
+                    Price50 = 50,
+                    Price100 = 50
+                },
+                new Product
+                {
+                    Id = 2,
+                    Title = "Drama",
+                    ISBN = "1",
+                    Description = "desc2"
+                    ,
+                    Author = "mahdi2",
+                    ListPrice = 50,
+                    Price = 50,
+                    Price50 = 50,
+                    Price100 = 50
+                },
+               new Product
+               {
+                   Id = 3,
+                   Title = "horor",
+                   ISBN = "1",
+                   Description = "desc3"
+                    ,
+                   Author = "mahdi3",
+                   ListPrice = 50,
+                   Price = 50,
+                   Price50 = 50,
+                   Price100 = 50
+               });
         }
     }
 }
---
+++ BookStoreDataAccess/Migrations/20240215205205_ProductTableAndSeed.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookStore.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductTableAndSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListPrice = table.Column<double>(type: "float", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Price50 = table.Column<double>(type: "float", nullable: false),
+                    Price100 = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Author", "Description", "ISBN", "ListPrice", "Price", "Price100", "Price50", "Title" },
+                values: new object[,]
+                {
+                    { 1, "mahdi1", "desc1", "1", 50.0, 50.0, 50.0, 50.0, "Action" },
+                    { 2, "mahdi2", "desc2", "1", 50.0, 50.0, 50.0, 50.0, "Drama" },
+                    { 3, "mahdi3", "desc3", "1", 50.0, 50.0, 50.0, 50.0, "horor" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
